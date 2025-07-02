9ed708dc53b0b53be1873bb23e2e216d6f882a51
ILindsley <ian.lindsley@unfoldingword.org>
2025-07-02 08:32:21 -0400

    removing potentially unsecure endpoints
    removing potentially unsecure endpoints

--- bt_servant.py
+++ bt_servant.py
@@ -6,13 +6,11 @@ from collections import defaultdict
 from fastapi import FastAPI, Request
 from fastapi.responses import Response
 from fastapi.staticfiles import StaticFiles
-from fastapi import HTTPException
 from pathlib import Path
 from brain import create_brain
 from logger import get_logger
 from config import config
 from pydantic import BaseModel
-from db import add_knowledgebase_doc, delete_knowledgebase_doc, update_knowledgebase_doc
 
 app = FastAPI()
 open_ai_client = OpenAI()
@@ -83,49 +81,6 @@ async def handle_meta_webhook(request: Request):
         logger.error("Error handling Meta webhook payload", exc_info=True)
 
     return Response(status_code=200)
-
-
-@app.post("/insert")
-async def insert_entry(entry: KnowledgeBaseEntry):
-    try:
-        doc_info = {
-            "question_or_prompt": entry.question_or_prompt,
-            "context_for_expected_response": entry.context_for_expected_response
-        }
-        doc_text = json.dumps(doc_info)
-        logger.info('received new knowledge base doc:\n\n%s', doc_text)
-        chroma_id = add_knowledgebase_doc(doc_text)
-        logger.info("returning chroma_id: %s", chroma_id)
-        return {"knowledgebase_id": chroma_id}
-    except Exception as e:
-        logger.error("Error while attempting to insert knowledgebase item.", exc_info=True)
-        raise HTTPException(status_code=500, detail="Insert failed")
-
-
-@app.put("/update/{doc_id}")
-def update_knowledgebase_entry(doc_id: str, updated_data: KnowledgeBaseEntry):
-    try:
-        doc_info = {
-            "question_or_prompt": updated_data.question_or_prompt,
-            "context_for_expected_response": updated_data.context_for_expected_response
-        }
-        doc_text = json.dumps(doc_info)
-        logger.info('received knowledge base doc for update:\n\n%s', doc_text)
-        update_knowledgebase_doc(doc_id, doc_text)
-        return {"status": "updated"}
-    except Exception as e:
-        logger.error("Error while attempting to update knowledgebase item.", exc_info=True)
-        raise HTTPException(status_code=500, detail="Update failed")
-
-
-@app.delete("/delete/{doc_id}")
-def delete_knowledgebase_entry(doc_id: str):
-    try:
-        delete_knowledgebase_doc(doc_id)
-        return {"status": "deleted"}
-    except Exception as e:
-        logger.error("Error while attempting to delete knowledgebase item.", exc_info=True)
-        raise HTTPException(status_code=500, detail="Deletion failed")
 
 
 def send_meta_text_message(to: str, text: str):
