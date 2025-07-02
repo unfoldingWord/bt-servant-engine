24f8f2caf7a256bf839969744c8a6fe5b5f36361
ILindsley <ian.lindsley@unfoldingword.org>
2025-07-02 09:45:23 -0400

    remove functionality to insert into the kb
    For now, remove functionality to insert into the knowledgebase. We will soon put this logic behind another secure endpoint.

--- db/__init__.py
+++ db/__init__.py
@@ -1,9 +1,6 @@
 from .user_db import get_user_db
 from .chroma_db import (
     get_chroma_collection,
-    add_knowledgebase_doc,
-    delete_knowledgebase_doc,
-    update_knowledgebase_doc
 )
 from .user import (
     get_user_chat_history,
@@ -17,8 +14,5 @@ __all__ = [
     "get_user_response_language",
     "set_user_response_language",
     "get_chroma_collection",
-    "add_knowledgebase_doc",
-    "update_knowledgebase_doc",
-    "delete_knowledgebase_doc"
 ]
 
--- db/chroma_db.py
+++ db/chroma_db.py
@@ -12,39 +12,12 @@ openai_ef = embedding_functions.OpenAIEmbeddingFunction(
                 api_key=config.OPENAI_API_KEY
             )
 _aquifer_chroma_db = chromadb.PersistentClient(path=str(DATA_DIR))
-_knowledge_base_collection = _aquifer_chroma_db.get_collection(name="knowledgebase", embedding_function=openai_ef)
 
 logger = get_logger(__name__)
 
 
 def get_chroma_collection(name) -> ClientAPI:
     return _aquifer_chroma_db.get_collection(name=name, embedding_function=openai_ef)
-
-
-def add_knowledgebase_doc(doc: str) -> str:
-    doc_id = str(get_next_doc_id(_knowledge_base_collection))
-    logger.info("using next doc id: %s", doc_id)
-    _knowledge_base_collection.add(
-        documents=[doc],
-        metadatas=[{"source": str("Knowledgebase User Override")}],
-        ids=[doc_id],
-    )
-    logger.info("successfully inserted:\n\n%s\n\ninto the db.", doc)
-    return doc_id
-
-
-def update_knowledgebase_doc(doc_id: str, doc: str) -> str:
-    _knowledge_base_collection.update(
-        documents=[doc],
-        metadatas=[{"source": str("Knowledgebase User Override")}],
-        ids=[doc_id],
-    )
-    logger.info("successfully updated doc_id %s with:\n\n%s\n\n.", doc_id, doc)
-
-
-def delete_knowledgebase_doc(doc_id: str):
-    _knowledge_base_collection.delete(ids=[doc_id])
-    logger.info("Deleted Chroma doc with ID: %s", doc_id)
 
 
 def get_next_doc_id(collection) -> int:
