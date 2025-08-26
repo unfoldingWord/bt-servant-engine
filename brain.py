6f29aea983ad18a1142b8087d2e96e73df1ecf77
Ian Lindsley <ian.lindsley@unfoldingword.org>
2025-08-26 09:56:26 -0400

    Add biblica_study_notes_key_terms collection
    Add biblica_study_notes_key_terms to the list of searchable collections.

--- brain.py
+++ brain.py
@@ -647,7 +647,8 @@ def determine_query_language(state: BrainState) -> dict:
         "aquifer_documents",
         "bsb",
         "tyndale_dictionary",
-        "uw_translation_words"
+        "uw_translation_words",
+        "biblica_study_notes_key_terms"
     ]
     if query_language in supported_collection_lang_map:
         stack_rank_collections.insert(1, f'aquifer_documents_{supported_collection_lang_map[query_language]}')
