c64d06adbdf241eef7ad74fe3c6b223a298e16c1
Ian Lindsley <ian.lindsley@unfoldingword.org>
2025-10-19 23:59:25 -0400

    bump version
    bump version

--- bt_servant_engine/__init__.py
+++ bt_servant_engine/__init__.py
@@ -1,6 +1,6 @@
 """Core package placeholder for upcoming onion architecture refactor."""
 
-BT_SERVANT_VERSION = "1.2.7"
+BT_SERVANT_VERSION = "1.2.8"
 BT_SERVANT_RELEASES_URL = "https://github.com/unfoldingWord/bt-servant-engine/releases"
 
 __all__ = ["BT_SERVANT_VERSION", "BT_SERVANT_RELEASES_URL"]
